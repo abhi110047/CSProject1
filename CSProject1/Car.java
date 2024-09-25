2e14bf85863cd5705dd85f7fea728438d5f0792f
Abhi <85300426+abhi110047@users.noreply.github.com>
2024-09-25 10:35:54 -0500

    Add files via upload

---
+++ CSProject1/Car.java
@@ -0,0 +1,35 @@
+import mayflower.*;
+/**
+ * Write a description of class Car here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Car extends Actor
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int lane;
+
+    /**
+     * Constructor for objects of class Car
+     */
+    public Car()
+    {
+        // initialise instance variables
+        setImage("img/car.png");
+        lane = 1;
+    }
+
+    
+    public int getLane() {
+        
+        return lane;
+    }
+    public void act(){
+        
+        if (Mayflower.is
+}
+
+  
+}
