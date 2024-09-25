af57c31c0df0b3bc18a3496b8edc7255f59d014a
Abhi <85300426+abhi110047@users.noreply.github.com>
2024-09-24 19:08:59 -0500

    Add files via upload

---
+++ CSProject1/MyMayflower.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class MyMayflower here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import mayflower.*;
+
+public class MyMayflower extends Mayflower 
+{
+    //Constructor
+    public MyMayflower()
+    {
+        
+        super("CS Project1", 600, 1067);
+    }
+
+    public void init()
+    {
+        
+        Mayflower.setFullScreen(false);
+        World w =  new MyWorld();
+        Mayflower.setWorld(w);
+    }
+}
+
