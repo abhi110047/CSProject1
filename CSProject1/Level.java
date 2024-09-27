05449a8b4f5433a74bcbf7657da851e75d3c9eb3
Abhi <85300426+abhi110047@users.noreply.github.com>
2024-09-27 10:30:35 -0500

    Add files via upload

---
+++ CSProject1/Level.java
@@ -0,0 +1,21 @@
+import mayflower.*;
+public class Level extends World 
+{
+    public int score;
+    public int lives;
+    public Level() 
+    {
+        
+    }  
+    
+    
+    
+    
+   
+    
+    
+    public void act()
+    {
+        
+    }
+}
---
+++ CSProject1/Level1.java
@@ -0,0 +1,32 @@
+import mayflower.*;
+/**
+ * Write a description of class Level1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Level1 extends Level
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Level1
+     */
+    public Level1()
+    {
+        // initialise instance variables
+        
+        setBackground("img/RacingBackground.png");
+        Car driver = new Car();
+        addObject(driver, (driver.getLane() - 1) * 97 + 11, 700);
+        
+        showText("Lane = " + driver.getLane(), 10, 30, Color.BLACK);
+    }
+
+    
+    public void act()
+    {
+       
+    }
+}
