7cf14068c289e2a4dc1ebcc64ab9ec3a025a3db5
Abhi <85300426+abhi110047@users.noreply.github.com>
2024-10-01 07:54:09 -0500

    Add files via upload

---
+++ CSProject1/Level.java
@@ -0,0 +1,22 @@
+import mayflower.*;
+public class Level extends World 
+{
+    public int score;
+    public int lives;
+
+    public Level() 
+    {
+        
+    }  
+    
+    
+    
+    
+   
+    
+    
+    public void act()
+    {
+        
+    }
+}
