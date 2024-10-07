fd7ed267bc50b5d82c314526f1cbd927dc53f078
yashm2980 <141870697+yashm2980@users.noreply.github.com>
2024-10-06 20:12:29 -0500

    Add files via upload

--- CSProject1/Car.java
+++ CSProject1/Car.java
@@ -1,4 +1,5 @@
 import mayflower.*;
+
 /**
  * Write a description of class Car here.
  *
@@ -13,6 +14,11 @@ public class Car extends Actor
     public int gas;
     public Timer c;
     public int multiplierIncrease;
+    private Animation animation;
+    private boolean goingLeft = false;
+    private int rightFrameNumber = 1;
+    
+    
 
     /**
      * Constructor for objects of class Car
@@ -20,7 +26,14 @@ public class Car extends Actor
     public Car()
     {
         // initialise instance variables
-        setImage("img/car.png");
+        //setImage("img/car.png");
+         MayflowerImage p = new MayflowerImage("img/car.png");
+
+        setImage(p);
+        String[] frames = new String[10];
+        
+    
+        
         lane = 1;
         c = new Timer(250000000);
         gas = 100;
@@ -47,13 +60,48 @@ public class Car extends Actor
         }
         return false;
     }
-    
+    public void moveRight(){
+        
+            for(int i = 1; i < 8; i++){
+                Timer x = new Timer(1000);
+                
+                MayflowerImage image = new MayflowerImage("rightTurnA/RightTurn" + i + ".png");
+                
+                if(x.isDone()){
+                   image.scale(0.5);
+                   setImage(image); 
+                }
+                
+                
+            
+            }
+            
+            
+            
+            lane+=1;
+            setLocation((getLane() - 1) * 82 + 98, 651);
+            for(int i = 8; i < 14; i++){
+                Timer y = new Timer(1000);
+                MayflowerImage image = new MayflowerImage("rightTurnA/RightTurn" + i + ".png");
+                if(y.isDone()){
+                   image.scale(0.5);
+                   setImage(image); 
+                }
+                
+                
+                
+            
+            }
+    }
     
     public void act(){
         
         if (Mayflower.isKeyPressed(Keyboard.KEY_RIGHT) && lane < 4){
-            lane+=1;
-            setLocation((getLane() - 1) * 82 + 98, 651);
+            
+            moveRight();
+            
+            
+            
             
             
         }
